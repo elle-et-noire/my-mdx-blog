db1369576dcf4aa2762291019ee1eab2a3701772
lomega <atibon3@gmail.com>
2024-12-18 07:25:36 +0900

    nice alignment of components (build succeeded)

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -29,26 +29,24 @@ export default async function PostPage(props: PostPageProps) {
   const { content, data } = GetPostBySlug(params.slug);
 
   return (
-    <div className="m-0 p-12 bg-[#a0bac8]">
-      <div className="prose max-w-none w-[32rem] md:w-[72rem] flex justify-center mx-auto p-10 rounded-lg bg-gray-100">
-        <div className="w-full font-[family-name:var(--font-noto-sans)]">
-          <h1 className="mt-8 mb-3 ml-8 font-[600]">{data.title}</h1>
-          <div className="ml-8 flex justify-start gap-2 font-[family-name:var(--font-kosugi-maru)] text-gray-400 font-[400] italic">
+    <div className="m-0 py-12 bg-[#a0bac8]">
+      <div className="prose max-w-none w-[40rem] md:w-[64rem] flex justify-center mx-auto pl-8 md:pl-16 pr-4 md:pr-8 pb-16 rounded-lg bg-gray-100">
+        <div className="w-[48rem] font-[family-name:var(--font-noto-sans)]">
+          <h1 className="mt-16 mb-3 font-[600]">{data.title}</h1>
+          <div className="flex justify-start gap-2 font-[family-name:var(--font-kosugi-maru)] text-gray-400 font-[400] italic">
             <div>
               <span className="pr-1">投稿日</span>{data.publish.toISOString().split('T')[0]}
               <span className="pl-2 pr-1">» 最終更新日</span>{data.lastUpdate.toISOString().split('T')[0]}
             </div>
           </div>
-          <div className="post bg-gray-100 p-8 font-[500] prose-h2:text-[#324e73] prose-h2:border-l-4 prose-h2:border-l-[#324e73] prose-h2:pl-3 prose-h2:py-1">
-            {/* <MathJaxContext> */}
+          <div className="post bg-gray-100 font-[500] prose-h2:text-[#324e73] prose-h2:border-l-4 prose-h2:border-l-[#324e73] prose-h2:pl-3 prose-h2:py-1">
             <MDXRemote source={content.replace(/\\\(/g, "<span className='inlinemath'>$\\hspace{0.2em}").replace(/\\\)/g, "\\hspace{0.2em}$</span>")} options={options} />
-            {/* </MathJaxContext> */}
           </div>
         </div>
         <div className="w-3/12 hidden md:block">
           <Toc />
         </div>
-        <Link href="/" className="h-8 sticky top-0 pt-1">
+        <Link href="/" className="h-8 sticky top-0 pt-4 md:pt-8">
           <div className="batsu"></div>
         </Link>
       </div>
