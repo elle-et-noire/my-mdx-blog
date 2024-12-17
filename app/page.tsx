7c84f27d5833ea388814916e343605ea711bf944
lomega <atibon3@gmail.com>
2024-12-18 07:43:14 +0900

    slight reduction of css (build succeeded)

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   const posts = GetAllPosts();
   return (
     <div className="min-h-screen bg-[#a0bac8] font-[family-name:var(--font-noto-sans)]">
-      <main className="flex flex-col items-center sm:items-start bg-[#a0bac8] py-16 w-[36rem] mx-auto">
+      <main className="flex flex-col items-center bg-[#a0bac8] py-16 md:w-[48rem] w-full mx-auto px-16">
         {posts.map((post) => (
           <PostCard
             key={post.slug}
             post={post}
             widthClass="w-full"
-            mainClassName="bg-[#edf1f4] text-[#344a5f] py-6 pl-6 rounded-md shadow-[0_1px_1px_1px_rgba(0,0,0,0.5)] overflow-hidden my-3"
-            textSize="text-base md:text-xl"
+            mainClassName="bg-[#edf1f4] text-[#344a5f] p-6 pl-6 rounded-md shadow-[0_1px_1px_1px_rgba(0,0,0,0.5)] overflow-hidden my-3"
+            textSize="text-lg md:text-xl"
           />
         ))}
       </main>
