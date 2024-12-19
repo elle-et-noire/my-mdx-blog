f37e9787932790750e13f0816bae9035b2786ac2
lomega <atibon3@gmail.com>
2024-12-19 09:04:45 +0900

    blur background (build succeeded)

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,21 @@ export default function Home() {
   const posts = GetAllPosts();
   return (
     <div className="min-h-screen bg-[#a0bac8] font-[family-name:var(--font-noto-sans)]">
-      <main className="flex flex-col items-center bg-[#a0bac8] py-16 md:w-[48rem] w-full mx-auto px-16">
-        {posts.map((post) => (
-          <PostCard
-            key={post.slug}
-            post={post}
-            widthClass="w-full"
-            mainClassName="bg-[#edf1f4] text-[#344a5f] p-6 pl-6 rounded-md shadow-[0_1px_1px_1px_rgba(0,0,0,0.5)] overflow-hidden my-3"
-            textSize="text-lg md:text-xl"
-          />
-        ))}
+      <main>
+        <div className="sticky top-0 w-full text-center text-[#112b45] underline underline-offset-[12px] decoration-4 decoration-yellow-300 bg-[#fefefe] text-2xl font-system pt-4 pb-2 shadow-[0_1px_1px_1px_rgba(0,0,0,0.3)]">
+          記事一覧
+        </div>
+        <div className="flex flex-col gap-3 items-center pb-16 pt-8 md:w-[48rem] w-full mx-auto px-16">
+          {posts.map((post) => (
+            <PostCard
+              key={post.slug}
+              post={post}
+              widthClass="w-full"
+              mainClassName="bg-[#fefefe] text-[#324e73] p-6 pl-6 rounded-md shadow-[0_1px_0px_2px_rgba(128,167,180,1)] overflow-hidden"
+              textSize="text-lg md:text-xl"
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -29,8 +29,12 @@ export default async function PostPage(props: PostPageProps) {
   const { content, data } = GetPostBySlug(params.slug);
 
   return (
-    <div className="min-h-screen min-w-max m-0 py-12 bg-[#a0bac8]">
-      <div className="prose max-w-none w-[40rem] md:w-[64rem] flex justify-center mx-auto pl-8 md:pl-16 pr-4 md:pr-8 pb-16 rounded-lg bg-gray-100">
+    <div className="min-h-screen min-w-max m-0 pb-12 bg-[#a0bac8]">
+      <div className="z-0 fixed top-0 w-full p-0 top-0 left-0 right-0 text-center text-[#112b45] underline underline-offset-[12px] decoration-4 decoration-yellow-300 bg-[#fefefe] text-2xl font-system pt-4 pb-2 shadow-[0_1px_1px_1px_rgba(0,0,0,0.3)]">
+        記事一覧
+      </div>
+      <div className="z-10 fixed top-0 left-0 right-0 size-full bg-gray-400 backdrop-filter backdrop-blur-[3px] bg-opacity-10"></div>
+      <div className="z-20 relative prose max-w-none w-[40rem] md:w-[64rem] flex justify-center mx-auto mt-8 pl-8 md:pl-16 pr-4 md:pr-8 pb-16 rounded-lg bg-[#fefefe] shadow-[0_0px_3px_0px_rgba(128,128,128,0.5)]">
         <div className="w-[48rem] font-[family-name:var(--font-noto-sans)]">
           <h1 className="mt-16 mb-3 font-[600]">{data.title}</h1>
           <div className="flex justify-start gap-2 font-[family-name:var(--font-kosugi-maru)] text-gray-400 font-[400] italic">
@@ -39,7 +43,7 @@ export default async function PostPage(props: PostPageProps) {
               <span className="pl-2 pr-1">» 最終更新日</span>{data.lastUpdate.toISOString().split('T')[0]}
             </div>
           </div>
-          <div className="post bg-gray-100 font-[500] prose-h2:text-[#324e73] prose-h2:border-l-4 prose-h2:border-l-[#324e73] prose-h2:pl-3 prose-h2:py-1">
+          <div className="post font-[500] prose-h2:text-[#324e73] prose-h2:border-l-4 prose-h2:border-l-[#324e73] prose-h2:pl-3 prose-h2:py-1">
             <MDXRemote
               source={content.replace(/\\\(/g, "<span className='inlinemath'>$\\hspace{0.2em}").replace(/\\\)/g, "\\hspace{0.2em}$</span>")}
               options={options}
